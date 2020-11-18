b1d2e4e113629534b3fea17747468da4f56f1a9c
aaaaaaaaaaa <aaaaa>
2020-11-18 18:04:04 +0800

    Initial Commit

---
+++ Food2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food2
+//
+//  Created by User09 on 2020/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food2/Food2App.swift
@@ -0,0 +1,17 @@
+//
+//  Food2App.swift
+//  Food2
+//
+//  Created by User09 on 2020/11/18.
+//
+
+import SwiftUI
+
+@main
+struct Food2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
